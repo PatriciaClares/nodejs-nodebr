18756652c143541dd254c3099243a337059b7524
patriciaClares <dev.patricia.c@gmail.com>
2020-06-10 14:43:56 -0300

    add database

--- 06.cli/database.js
+++ 06.cli/database.js
@@ -45,5 +45,37 @@ class Database{
         const dadosFiltrados = dados.filter(item =>(id ? item.id === id : true))
         return dadosFiltrados
     }
+    async remover(id){
+        if(!id){
+            return await this.escreverArquivo([])
+        }
+
+        const dados = await this.obterDadosArquivos()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice === -1 ){
+            throw Error('O usuario informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados) 
+    }
+    async atualizar(id, modificações){
+        const dados = await this.obterDadosArquivos()
+        const indice = dados.findIndex(item => item.id ===parseInt(id))
+
+        if(indice === -1){
+            throw Error('O heroi informado não existe')
+        }
+
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificações
+        }
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados, 
+            objetoAtualizar])
+    }
 }
 module.exports = new Database()
