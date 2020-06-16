35c5543bb266cb584e77238cfebf10d93f7552bd
patriciaClares <dev.patricia.c@gmail.com>
2020-06-16 12:36:37 -0300

    removendo class ContextStrategy

--- 07.multi-db/src/strategy.js
+++ 07.multi-db/src/strategy.js
@@ -22,6 +22,8 @@ class ICrud {
     }
 }
 
+module.exports = ICrud
+
 class MongoDB extends ICrud{
     constructor(){
         super()
@@ -42,28 +44,8 @@ class Postgres extends ICrud {
     }
 }
 
-class ContextStrategy {
-    constructor(strategy){
-        this._database = strategy
-    }
-
-    create(item){
-        return this._database.create(item)
-    }
-
-    read(item){
-        return this._database.read(item)
-    }
-
-    update(id, item){
-        return this._database.update(id, item)
-    }
-
-    delete(id){
-        return this._database.delete(id)
-    }
-
-}
-
 const contextMongo = new ContextStrategy(new MongoDB())
 contextMongo.create()
+
+const contextPostgres =  new ContextStrategy(new Postgres())
+contextPostgres.create()
