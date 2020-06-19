4b1898c7b1a53e6ef5592637ab60bea1040bbfc8
patriciaClares <dev.patricia.c@gmail.com>
2020-06-19 17:31:08 -0300

    add referencia do sequelize

--- 08.multi-db postgres/src/db/strategies/postgres.js
+++ 08.multi-db postgres/src/db/strategies/postgres.js
@@ -1,12 +1,28 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
 
 class Postgres extends ICrud {
     constructor(){
         super()
+        this._driver = null
+        this._herois = null
     }
 
     create(item){
         console.log('O item foi salvo em Postgres')
+    }
+
+    _connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'patriciaClares',
+            'minhasenhasecreta', 
+            {
+                host:'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+            }
+        )
     }
 }
 
