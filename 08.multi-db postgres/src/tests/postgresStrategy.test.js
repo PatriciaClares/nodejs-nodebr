dc7cf63200ab3930549eaf7e541ab40d91b5c7f6
patriciaClares <dev.patricia.c@gmail.com>
2020-06-22 17:09:59 -0300

    add teste cadastrar

--- 08.multi-db postgres/src/tests/postgresStrategy.test.js
+++ 08.multi-db postgres/src/tests/postgresStrategy.test.js
@@ -3,11 +3,22 @@ const Postgres = require('../db/strategies/postgres')
 const Context = require('../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres())
+const MOCK_HEROI_CADASTRAR =    {
+    nome: 'Gaviao Negro',
+    poder: 'flexas'
+}
 
 describe('Postgres Strategy', function() {
     this.timeout(Infinity)
+    before(async function (){
+        await context.connect()
+    })
     it('PostgresSQL Connection', async function() {
         const result = await context.isConnected()
         assert.equal(result, true)
+    })
+    it('cadastrar', async function() {
+        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.equal(result, MOCK_HEROI_CADASTRAR)
     })
 })
