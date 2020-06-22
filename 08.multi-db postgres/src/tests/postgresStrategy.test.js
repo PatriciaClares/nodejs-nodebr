adb54250a8b2188f3d45f0b60e8b56b083d26954
patriciaClares <dev.patricia.c@gmail.com>
2020-06-22 20:12:26 -0300

    add teste para atualizar

--- 08.multi-db postgres/src/tests/postgresStrategy.test.js
+++ 08.multi-db postgres/src/tests/postgresStrategy.test.js
@@ -7,11 +7,17 @@ const MOCK_HEROI_CADASTRAR = {
     nome: 'Gaviao Negro',
     poder: 'flexas'
 }
+const MOCK_HEROI_ATUALIZAR = {
+    nome: 'Batiman',
+    poder: 'dinheiro'
+}
 
 describe('Postgres Strategy', function() {
     this.timeout(Infinity)
     before(async function (){
         await context.connect()
+        await context.delete()
+        await context.create(MOCK_HEROI_ATUALIZAR)
     })
     it('PostgresSQL Connection', async function() {
         const result = await context.isConnected()
@@ -27,4 +33,16 @@ describe('Postgres Strategy', function() {
         delete result.id
         assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
     })
+    it('atualizar', async function() {
+        const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_ATUALIZAR.nome })
+        const novoItem = {
+            ...MOCK_HEROI_ATUALIZAR,
+            nome: 'Mulher maravilha'
+        }
+        await context.update(itemAtualizar.id, novoItem)
+        const [itemAtualizado] = await context.read({ id: itemAtualizar.id })
+        delete itemAtualizado.id
+        assert.deepEqual(itemAtualizado, novoItem)
+    })
+
 })
