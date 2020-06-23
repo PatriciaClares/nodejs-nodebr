d17685932b97897771c930a3e2615ed1be5c234b
patriciaClares <dev.patricia.c@gmail.com>
2020-06-23 17:24:04 -0300

    add isConnect em mongodb

--- 09.multi-db postgres-mongodb/src/db/strategies/mongodb.js
+++ 09.multi-db postgres-mongodb/src/db/strategies/mongodb.js
@@ -1,9 +1,26 @@
 const ICrud = require('./interfaces/interfaceCrud')
 const Mongoose = require('mongoose')
+const STATUS = {
+    0: 'Desconectado',
+    1: 'Conectado',
+    2: 'Conectando',
+    3: 'Desconectando' 
+}
 
 class MongoDB extends ICrud{
     constructor(){
         super()
+    }
+
+    async isConnected() {
+        const state = STATUS[connection.readyState]
+        if(state === 'Conectado') return state;
+
+        if(state !== 'Conectando') return state;
+
+        await new Promise(resolve => setTimeout(resolve, 1000))
+
+        return STATUS[Mongoose.connection.readyState]
     }
 
     connect() {
