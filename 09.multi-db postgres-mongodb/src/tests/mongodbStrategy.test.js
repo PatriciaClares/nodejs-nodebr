59f5b45cdfbcabdd44cffa3130300d24aa621662
patriciaClares <dev.patricia.c@gmail.com>
2020-06-24 14:07:25 -0300

    add teste cadastrar

--- 09.multi-db postgres-mongodb/src/tests/mongodbStrategy.test.js
+++ 09.multi-db postgres-mongodb/src/tests/mongodbStrategy.test.js
@@ -2,15 +2,23 @@ const assert = require('assert')
 const MongoDb = require('../db/strategies/mongodb')
 const Context = require('../db/strategies/base/contextStrategy')
 
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Mulher maravilha',
+    poder: 'Laço'
+}
 const context = new Context(new MongoDb())
 describe('MongoDB Suite de testes', () => {
     before(async () => {
         await context.connect()
     })
-    it.only('verificar conexao', async () => {
+    it('verificar conexao', async () => {
         const result = await context.isConnected()
         const expected = 'Conectado'
 
         assert.deepEqual(result, expected)
+    })
+    it('cadastrar', async () => {
+        const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
     })
 })
