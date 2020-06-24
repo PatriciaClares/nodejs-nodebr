27ef04a4b637c7b8b2c0d643ae6ebaf62c3c43b9
patriciaClares <dev.patricia.c@gmail.com>
2020-06-24 18:59:15 -0300

    refatorando before do teste

---
+++ 10.multi-db-refactoring-mongodb/src/tests/mongodbStrategy.test.js
@@ -0,0 +1,53 @@
+const assert = require('assert')
+const MongoDb = require('../db/strategies/mongodb/mongodb')
+const HeroiSchema = require('../db/strategies/mongodb/schemas/heroisSchema')
+const Context = require('../db/strategies/base/contextStrategy')
+
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Mulher maravilha',
+    poder: 'Laço'
+}
+const MOCK_HEROI_DEFAULT = {
+    nome: `Homem-Aranha-${Date.now()}`,
+    poder: 'Super teia'
+}
+const MOCK_HEROI_ATUALIZAR = {
+    nome: `Super-Shock-${Date.now()}`,
+    poder: 'Eletrecidade'
+}
+let MOCK_HEROI_ID = ''
+let context = {}
+describe('MongoDB Suite de testes', () => {
+    before(async () => {
+        const connection = MongoDb.connect()
+        context = new Context(new MongoDb(connection, HeroiSchema))
+
+        await context.create(MOCK_HEROI_DEFAULT)
+        const result = await context.create(MOCK_HEROI_ATUALIZAR)
+        MOCK_HEROI_ID = result._id
+    })
+    it('verificar conexao', async () => {
+        const result = await context.isConnected()
+        const expected = 'Conectado'
+
+        assert.deepEqual(result, expected)
+    })
+    it('cadastrar', async () => {
+        const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+    it('listar', async () => {
+        const [{nome,poder}] = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
+        assert.deepEqual({nome, poder}, MOCK_HEROI_DEFAULT)
+    })
+    it('atualizar', async () => {
+        const result = await context.update(MOCK_HEROI_ID, {
+            nome:'Pernalonga'
+        })
+        assert.deepEqual(result.nModified, 1)
+    })
+    it('remover', async () => {
+        const result = await context.delete(MOCK_HEROI_ID)
+        assert.deepEqual(result.n, 1)
+    })
+})
